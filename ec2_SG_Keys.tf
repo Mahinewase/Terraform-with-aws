ba4cfcc241cb8711702e0f30cc5f0bb153b09f09
Mahesh Newase <106880588+Mahinewase@users.noreply.github.com>
2023-01-23 12:52:44 +0530

    Create ec2_SG_Keys.tf

---
+++ ec2_SG_Keys.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+ region   = "ap-south-1"
+ access_key = "AKIA4EQ3ZTTB2WRAXMVL"
+ secret_key = "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"
+}
+resource "aws_instance" "webserver" {
+ ami           = "ami-0cca134ec43cf708f"
+ instance_type      = "t2.micro"
+ key_name        = aws_key_pair.demokey.id
+vpc_security_group_ids = [aws_security_group.ssh-access.id]
+}
+resource "aws_key_pair" "demokey" {
+ key_name  = "demokey"
+ public_key = file ("/Users/admin/Terraform/keys.pub")
+}
+resource "aws_security_group" "ssh-access" {
+   name = "ssh-access"
+   ingress {
+   from_port    = 22
+   to_port     = 22
+   protocol     = "tcp"
+   cidr_blocks   = ["0.0.0.0/0"]
+ }
+}
+ output publicip{
+  value = aws_instance.webserver.public_ip
+}
