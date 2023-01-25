002ec78e3e8ee0b3979f865f0f4ef90ed91aae96
Mahesh Newase <106880588+Mahinewase@users.noreply.github.com>
2023-01-25 12:54:04 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA4EQ3ZTTBX5GOJKHQ"
-  secret_key = "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"
+  access_key = "Access_key_ID"
+  secret_key = "Secret_key_ID"
 }
