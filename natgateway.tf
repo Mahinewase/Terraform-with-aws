dae64bbbf39358baa4e6da6f4770a4c1eaafb32e
Mahesh Newase <106880588+Mahinewase@users.noreply.github.com>
2023-05-22 13:01:14 +0530

    Update natgateway.tf

--- natgateway.tf
+++ natgateway.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "NATGW" {
-  allocation_id = "eipalloc-0d49a109553468adc"
-  subnet_id     = "subnet-00c867f1c58b8291e"
+  allocation_id = "eipalloc-0d49----68adc"
+  subnet_id     = "subnet-00c8----------91e"
 
   tags = {
     Name = "NATGW"
